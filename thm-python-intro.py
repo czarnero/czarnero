cd5de27f78959ed4eb9eefea60d9db27dd3e41a6
czarnero <30670514+czarnero@users.noreply.github.com>
2021-03-13 14:53:29 +0100

    Update thm-python-intro.py

--- thm-python-intro.py
+++ thm-python-intro.py
@@ -1,8 +1,9 @@
 from base64 import *
 # 5x decode base16, 5x decode base32, 5x decode base64
-# solves THM's Intro to Python challenge (Task 12)
+# solves THM's Intro to Python challenge (Task 12) but allows for any file to be read, not just encodedflag.txt
 
-flag = open("encodedflag.txt", "r").read()
+user_input = input("Enter filename ")
+flag = open(user_input, "r").read()
 
 for i in range(0,5):
     flag = b16decode(flag)
